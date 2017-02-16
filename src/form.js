98f99230b6ef8d32a029193b02da5aa634bbc293
ktabriga <jhonatancatabriga@gmail.com>
2017-02-16 16:08:19 -0200

    Altera formulário para não passar botão de render por prop

--- src/form.js
+++ src/form.js
@@ -2,60 +2,66 @@ import React, {PropTypes, Component} from 'react';
 import {View} from 'react-native';
 
 export default class Form extends Component {
-  _listeners = [];
+ _listeners = [];
 
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
+ constructor(props) {
+   super(props);
+   this.submit = this.submit.bind(this);
+ }
 
-  static PropTypes = {
-    onSuccess: PropTypes.func,
-    onFail: PropTypes.func,
-    submitButton: PropTypes.element.isRequired
-  }
+ static PropTypes = {
+   onSuccess: PropTypes.func,
+   onFail: PropTypes.func,
+   submitButton: PropTypes.element.isRequired
+ }
 
-  static childContextTypes = {
-    addFormListener: PropTypes.func,
-    removeFormListener: PropTypes.func
-  }
+ static childContextTypes = {
+   addFormListener: PropTypes.func,
+   removeFormListener: PropTypes.func
+ }
 
-  getChildContext() {
-    return {
-      addFormListener: this.addFormListener.bind(this),
-      removeFormListener: this.removeFormListener.bind(this)
-    }
-  }
+ getChildContext() {
+   return {
+     addFormListener: this.addFormListener.bind(this),
+     removeFormListener: this.removeFormListener.bind(this)
+   }
+ }
 
-  addFormListener(ln) {
-    this._listeners.push(ln);
-  }
+ addFormListener(ln) {
+   this._listeners.push(ln);
+ }
 
-  removeFormListener(ln) {
-    this._listeners = this._listeners.filter(l => l  !== ln);
-  }
+ removeFormListener(ln) {
+   this._listeners = this._listeners.filter(l => l  !== ln);
+ }
 
-  submit() {
-    const defaultFn = () => {};
-    const {onFail = defaultFn, onSuccess = defaultFn} = this.props;
+ submit() {
+   const defaultFn = () => {};
+   const {onFail = defaultFn, onSuccess = defaultFn} = this.props;
 
-    const stop = this._listeners.reduce((stop, _listener) => {
-      return _listener() || stop;
-    }, false);
-    if (stop) {
-      return onFail();
-    };
-    onSuccess();
-  }
+   const stop = this._listeners.reduce((stop, _listener) => {
+     return _listener() || stop;
+   }, false);
+   if (stop) {
+     return onFail();
+   };
+   onSuccess();
+ }
 
-  render() {
-    const {submitButton = () => {}, children, style, disableSubmit} = this.props;
-
-    return (
-        <View style={style}>
-          {children}
-          {submitButton(this.submit, disableSubmit)}
-        </View>
-    )
-  }
+ render() {
+   const {submitButton = () => {}, children, style, disableSubmit} = this.props;
+   const finalChildren = React.Children.map(children, child => {
+     if (child.props.submit) {
+       return React.cloneElement(child, {
+         onPress: this.submit
+       });
+     }
+     return React.cloneElement(child);
+   })
+   return (
+       <View style={style}>
+         {finalChildren}
+       </View>
+   )
+ }
 }
